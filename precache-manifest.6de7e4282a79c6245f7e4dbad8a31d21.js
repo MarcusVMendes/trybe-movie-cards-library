ee9b7bceb380f1c1df837073da2072e64afef9bf
MarcusVMendes <MarcusVMendes@users.noreply.github.com>
2022-05-25 14:57:20 +0000

    deploy: a052e79a62403314bfc5befbac10cca5bcb41d51

---
+++ precache-manifest.6de7e4282a79c6245f7e4dbad8a31d21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ea2328b708b2f91cacf5d104aae4201",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0e973c7221005ca8ec77",
+    "url": "./static/css/main.e2f7de8e.chunk.css"
+  },
+  {
+    "revision": "0d4f78d6a282aeb41de1",
+    "url": "./static/js/2.9a6a2542.chunk.js"
+  },
+  {
+    "revision": "0e973c7221005ca8ec77",
+    "url": "./static/js/main.f264fda9.chunk.js"
+  },
+  {
+    "revision": "d0a1a38ad8c008272a9c",
+    "url": "./static/js/runtime-main.f758997e.js"
+  }
+]);
